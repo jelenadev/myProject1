aedbfb807026359a3d09eff598f942e97e439c3a
jelenadev <43375451+jelenadev@users.noreply.github.com>
2021-10-25 01:30:47 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,1 +1,102 @@
+#include "main.h";
 
+class StudentuDuomenys
+{
+    string studentoVardas, studentoPavarde;
+    int namuDarbuSk, namuDarbuRez, egzaminoRez;
+    double galutPazymys;
+    vector <double> skaicius;
+
+public:
+    //konstruktorius be parametrų
+    StudentuDuomenys()
+    {
+    };
+
+   //konstruktorius su parametrais
+    StudentuDuomenys(string studVa, string studPa, int ndSk, int ndRez, int egRez, double gaPaz)
+    {
+            studentoVardas = studVa;
+            studentoPavarde = studPa;
+            namuDarbuSk = ndSk;
+            namuDarbuRez = ndRez;
+            egzaminoRez = egRez;
+    }
+
+    //kopijavimo konstruktorius
+    StudentuDuomenys(StudentuDuomenys &kopija)
+    {
+            studentoVardas = kopija.studentoVardas;
+            studentoPavarde = kopija.studentoPavarde;
+            namuDarbuSk = kopija.namuDarbuSk;
+            namuDarbuRez = kopija.namuDarbuRez;
+            egzaminoRez = kopija.egzaminoRez;
+    }
+    /* //destruktorius
+     ~StudentuDuomenys(); */
+
+    void ivestiDuomenis(string, string, int, int, int);
+    void spausdinti();
+    void spausdintiGalutPagalVidurki();
+    void spausdintiGalutPagalMediana();
+};
+
+void StudentuDuomenys::ivestiDuomenis(string studVa, string studPa, int ndSk, int ndRez, int egRez)
+{
+            studentoVardas = studVa;
+            studentoPavarde = studPa;
+            namuDarbuSk = ndSk;
+            namuDarbuRez = ndRez;
+            egzaminoRez = egRez;
+}
+
+void StudentuDuomenys::spausdintiGalutPagalVidurki()
+{
+    double vidurkis = ((double)namuDarbuRez + (double)egzaminoRez)/(double)namuDarbuSk;
+    double galutPazymys = 0.4 * vidurkis + 0.6 * egzaminoRez;
+    cout << "Vardas    " << "Pavarde    "<<"Galutinis pazymys pagal vidurki "<<endl;
+    cout <<"------------------------------------------------"<<endl;
+    cout << studentoVardas<< "   " <<studentoPavarde<<"   " <<setprecision(3)<<galutPazymys<<endl;
+}
+
+ void StudentuDuomenys::spausdintiGalutPagalMediana()
+{
+double mediana;
+
+    sort(skaicius.begin(), skaicius.end());
+
+    for (int i = 0; i < skaicius.size(); ++i)
+    {
+    if (skaicius.size() % 2)
+   {
+       mediana = skaicius[skaicius.size() / 2];
+   }
+    else
+{
+    mediana = (skaicius[skaicius.size() / 2] + skaicius[skaicius.size() / 2 - 1]) / 2;
+}
+    }
+
+    double galutPazymys = 0.4 * mediana + 0.6 * egzaminoRez;
+    cout << "Vardas    " << "Pavarde    "<<"Galutinis pazymys pagal mediana "<<endl;
+    cout <<"------------------------------------------------"<<endl;
+    cout << studentoVardas<< "   " <<studentoPavarde<<"   " <<setprecision(3)<<galutPazymys<<endl;
+}
+
+int main()
+{
+    cout << "START" << endl;
+    StudentuDuomenys sd;
+    sd.ivestiDuomenis("Jonas", "Jonaitis", 3, 10, 9);
+    sd.spausdintiGalutPagalVidurki();
+     sd.spausdintiGalutPagalMediana();
+
+    sd.ivestiDuomenis("Petras", "Petraitis", 3, 8, 8);
+    sd.spausdintiGalutPagalVidurki();
+    sd.spausdintiGalutPagalMediana();
+
+    sd.ivestiDuomenis("Pranas", "Pranaitis", 3, 9, 10);
+    sd.spausdintiGalutPagalVidurki();
+    sd.spausdintiGalutPagalMediana();
+    cout << "END" << endl;
+}
---
+++ main.h
@@ -0,0 +1,18 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::endl;
+using std::setprecision;
+using std::vector;
+using namespace std;
+
+#endif // MAIN_H_INCLUDED
